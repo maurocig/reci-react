06d3b6477037bd6073fc7bd34fb91a90ea3bf910
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-06-25 15:36:33 -0300

    git add *

---
+++ src/Components/ItemCount/ItemCount.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './ItemCount.css';
+
+const ItemCount = ({ myNum }) => {
+	const [count, setCount] = useState(0);
+
+	function addOne() {
+		setCount(count + 1);
+	}
+
+	function substractOne() {
+		count > 0 ? setCount(count - 1) : setCount(count);
+	}
+
+	function addToCart() {
+		if (count > 0) {
+			console.log(`se agregaron ${count} items al carrito.`);
+		}
+	}
+
+	return (
+		<>
+			<h2>Item Count</h2>
+			<button onClick={substractOne}>-</button>
+			<h3>{count}</h3>
+			<button onClick={addOne}>+</button>
+			<button id='addBtn' onClick={addToCart}>Agregar al carrito</button>
+		</>
+	)
+}
+
+export default ItemCount;
