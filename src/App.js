7bee6dfae8958d8379681ec89ff6a2d77c8a5826
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-06-25 15:35:52 -0300

    contador 3

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar/Navbar.js';
-import Item from './Components/Item';
-import Input from './Components/Input';
-import Title from './Components/Title';
-import { useState } from 'react';
+import Navbar from './Components/Navbar/Navbar.jsx';
+// import { useState } from 'react';
 import ItemListContainer from './Components/ItemListContainer';
-import { Contador } from './Components/Contador';
+import ItemCount from './Components/ItemCount/ItemCount';
 
 // Componente contenedor / State components
 function App({ name, children, app, saludaPapa, }) {
 	// variable o atributo del estado react
-	const [data] = useState(['Ingrese su nombre', 'Ingrese su apellido', 'Ingrese su DNI']);
 
-	const saludoYoHijo = () => console.log('hola, saludo yo hijo')
 
 	return (
 		<div className="App">
 			<Navbar style={{ backgroundColor: 'blue' }} />
 			<ItemListContainer greeting="Esto es un mensaje para cumplir con la consigna." />
-
-			<button onClick={saludoYoHijo}>Saludar hijo</button>
-			<button onClick={saludaPapa}>Saludar papa</button>
-
-			<Item value={'CLICK ACÁ.'} />
-			<Title name="Reci Importaciones Ltda." />
-
-			{
-				data.map((item) => {
-					return (
-						<Input key={item} placeholder={item} />
-						// los items del map deben tener un key o te tira error.
-					)
-				})
-			}
-
-			<Contador />
+			<ItemCount></ItemCount>
 
 			{children}
 
--- src/Components/Input.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Input = (props) => (
-	<input type="text" placeholder={props.placeholder} />
-)
-
-export default Input;
