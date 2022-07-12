7f8c8d1c10fe484e3d720f42b92b18c08f17d2dd
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-07-11 22:39:21 -0300

    rutas resueltas

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -18,8 +18,11 @@ const ItemDetailContainer = ({ props }) => {
 				/* const data = await response.json(); */
 				/* setProduct(data[0]); */
 
+				const productoDetalle = ProductosIniciales.find((producto) => producto.id == itemId);
 
-				setProduct(ProductosIniciales[itemId]);
+				// console.log(productoDetalle)
+				setProduct(productoDetalle);
+
 				// console.log(itemId)
 
 			}
