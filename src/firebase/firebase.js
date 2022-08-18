4945bc1a9b5c2a31892292e98b9af15e2d37033c
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-08-18 13:13:17 -0300

    env configurado correctamente

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,19 +1,29 @@
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+// console.log(process.env)
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyD1VLJPcpLSmAPv_q1dn2xIHKysVRbm9ss",
-	authDomain: "tonal-asset-268817.firebaseapp.com",
-	projectId: "tonal-asset-268817",
-	storageBucket: "tonal-asset-268817.appspot.com",
-	messagingSenderId: "182550924958",
-	appId: "1:182550924958:web:ac8af5ce71ff06934bf58f",
-	measurementId: "G-67EQ9RB8QW"
+
+	// apiKey: process.env.apiKey,
+	// authDomain: process.env.authDomain,
+	// projectId: process.env.projectId,
+	// storageBucket: process.env.storageBucket,
+	// messagingSenderId: process.env.messagingSenderId,
+	// appId: process.env.appId,
+	// measurementId: process.env.measurementId
+
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
